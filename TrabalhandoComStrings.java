b02f7055f644a49b4f26ca30536fbb5728769e11
Andre Chaves Lima <aclchaves@gmail.com>
2022-03-07 21:53:33 -0300

    implementação para a aula dia 08/03

---
+++ TrabalhandoComStrings.java
@@ -0,0 +1,16 @@
+public class TrabalhandoComStrings{
+
+    public static void main(String[] args){
+        int x = 10;
+        int y = 5;
+        //int z = x + y;
+        //System.out.println("Resultado: " + z);
+        //System.out.println("Resultado: " + x + y);
+        //System.out.println("Resultado: " + (x + y));
+
+        String nome = "Maria";
+        int idade = 30;
+
+        System.out.println(nome + " tem " + idade + " anos ");
+    }
+}
